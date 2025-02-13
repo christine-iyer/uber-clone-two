f0212757240bb31912fbd0fa9e8a8a53c0ce5b74
chrisIyer <hellohotbox420@gmail.com>
2025-02-13 18:40:32 -0500

    set up with nativewind working.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import './global.css';
 
 export default function App() {
@@ -8,6 +8,12 @@ export default function App() {
     <>
       <ScreenContent title="Home" path="App.tsx" />
       <StatusBar style="auto" />
+      <View className="mx-3 flex flex-1 flex-col items-start justify-center">
+        <Text className="text-xl font-bold text-red-500">Hello from NativeWind!</Text>
+        <Text className="mt-2 text-gray-600">
+          This is a test to check if Tailwind (NativeWind) is working correctly.
+        </Text>
+      </View>
     </>
   );
 }
