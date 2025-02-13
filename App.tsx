fb0ae35af570c623da005573fdc8bc8a9f10df6a
chrisIyer <hellohotbox420@gmail.com>
2025-02-13 13:07:54 -0500

    Home

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-
+import { View } from 'react-native';
 import './global.css';
 
 export default function App() {
