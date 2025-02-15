1616c637ca9999dc29dca250f227ef925b5afde1
chrisIyer <hellohotbox420@gmail.com>
2025-02-15 14:50:24 -0500

    i'm being so lame

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, Image, Animated, Easing } from 'react-native';
 import { Container } from 'components/Container';
-import pic from './assets/icon.png';
+import pic from './assets/theIcon.png';
 import './global.css';
 import { useEffect,useRef } from 'react';
 
@@ -28,14 +28,14 @@ export default function App() {
       <StatusBar style="auto" />
       <View className="mx-3 flex flex-1 flex-col items-start justify-center">
         <Text className="text-xl font-bold text-yellow-500">Hello from NativeWind!</Text>
-        <Text className="mt-2 text-green-600">
+        <Text className="mt-2 text-green-600 p-1">
           This is a test to check if Tailwind (NativeWind) is working correctly.
         </Text>
         <Container>
-          <View>
+          <View className="w-16 h-16">
            <Animated.Image 
            source={pic}
-           className={"w-22 h-22"}
+           className={"w-full h-full"}
            style={{transform: [{rotate:spin}]}}
            />
       </View>
